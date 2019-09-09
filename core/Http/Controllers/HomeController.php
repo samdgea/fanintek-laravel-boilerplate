f5cbba1ad95062d8106c2cf68c69076d2fa0a721
Abdilah Sammi <samdgea.id@gmail.com>
2019-09-09 14:56:22 +0700

    Change Password Module

--- core/Http/Controllers/HomeController.php
+++ core/Http/Controllers/HomeController.php
@@ -31,17 +31,64 @@ class HomeController extends Controller
      * Show the user profile info.
      * 
      * @param \Illuminate\Http\Request
+     * @param \Kris\LaravelFormBuilder\FormBuilder
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function profile(Request $request, FormBuilder $formBuilder)
     {
         $formChangePassword = $formBuilder->create(\Fanintek\Fantasena\Forms\Admin\UserChangePassword::class, [
             'method' => "POST",
-            'url' => route('profile')
+            'url' => route('change-password')
         ])->addBefore('password', 'old_password', 'password', [
             'attr' => ['placeholder' => '******'],
             'rules' => 'required|string|min:6|max:50'
         ]);
+
         return view('profile', compact('formChangePassword'));
+    }
+
+    /**
+     * Process change Password
+     * 
+     * @param \Illuminate\Http\Request
+     * @param \Kris\LaravelFormBuilder\FormBuilder
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function changePassword(Request $request, FormBuilder $formBuilder)
+    {
+        $formChangePassword = $formBuilder->create(\Fanintek\Fantasena\Forms\Admin\UserChangePassword::class)
+        ->addBefore('password', 'old_password', 'password', [
+            'attr' => ['placeholder' => '******'],
+            'rules' => 'required|string|min:6|max:50'
+        ]);
+        
+        if (!$formChangePassword->isValid())
+            return redirect()->back()->withErrors($formChangePassword->getErrors())->withInput();
+
+        $fields = $formChangePassword->getFieldValues();
+        $user = \Auth::user();
+
+        if (\Hash::check($fields['old_password'], $user->password) === false)
+            return redirect()->back()->with('splash-type', 'danger')->with('splash-message', 'Incorrect old password, please try again');
+
+        $user->password = \Hash::make($fields['password']);
+
+        if ($user->save())
+            return redirect()->route('manage.menu.index')->with('splash-type', 'success')->with('splash-message', 'Password was changed successfully');
+        
+        return redirect()->back()->with('splash-type', 'danger')->with('splash-message', 'There\'s something wrong when saving changes');
+        
+    }
+
+    /**
+     * Process change Profile
+     * 
+     * @param \Illuminate\Http\Request
+     * @param \Kris\LaravelFormBuilder\FormBuilder
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function changeProfile(Request $request, FormBuilder $formBuilder)
+    {
+
     }
 }
--- core/Routes/web.php
+++ core/Routes/web.php
@@ -19,5 +19,11 @@ Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('home', 'HomeController@index')->name('home');
-    Route::match(['get', 'post'], 'profile', 'HomeController@profile')->name('profile');
+    
+    Route::group(['prefix' => 'profile'], function() {
+        Route::get('/', 'HomeController@profile')->name('profile');
+
+        Route::post('changePassword', 'HomeController@changePassword')->name('change-password');
+        Route::post('changeProfile', 'HomeController@changeProfile')->name('change-profile');
+    });
 });
